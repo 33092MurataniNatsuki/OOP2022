33608cf91c19ffdf72eb79d08d5c1f04321f401e
33092村谷夏希 <33092@ojs.ac.jp>
2022-05-10 14:02:53 +0900

    休んだ分

--- Chapter02/SalesCalculater/SalesCounter.cs
+++ Chapter02/SalesCalculater/SalesCounter.cs
@@ -1,24 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SalesCalculater {
     public class SalesCounter {
-        private List<Sale> _sales; //csvファイルから読み込んだデータ
+        private IEnumerable<Sale> _sales;  //csvファイルから読み込んだデータ
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales)
+        public SalesCounter(string filePath)
         {
-            _sales = sales;
+            _sales = ReadSales(filePath);
         }
 
         //店舗別売り上げを求める
-        public Dictionary<string, int> GetPerStoreSales()
+        public IDictionary<string, int> GetPerStoreSales()
         {
-            Dictionary<string, int> dict = new Dictionary<string, int>();
-            foreach (Sale sale in _sales)
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _sales)
             {
                 if (dict.ContainsKey(sale.ShopName))
                     dict[sale.ShopName] += sale.Amount;
@@ -27,5 +28,25 @@ namespace SalesCalculater {
             }
             return dict;
         }
+
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        public static IEnumerable<Sale> ReadSales(string filePath)
+        {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] items = line.Split(',');
+                Sale sale = new Sale
+                {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+
     }
 }
