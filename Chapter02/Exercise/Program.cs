f13ee468c8411a7943f8f9eca286b329a21af10e
33092村谷夏希 <33092@ojs.ac.jp>
2022-05-10 15:18:49 +0900

    問題2.1.3完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -8,6 +8,26 @@ namespace Exercise {
     class Program {
         static void Main(string[] args)
         {
+            //2.1.3
+            //List<Song> songs = new List<Song>();
+
+            //var song1 = new Song("アンパンマン", "バイキンマン", 60);
+            //songs.Add(song1);
+
+            //var song2 = new Song("アンパンマン", "バイキンマン", 60);
+            //songs.Add(song2);
+
+            //var song3 = new Song("アンパンマン", "バイキンマン", 60);
+            //songs.Add(song3);
+
+            var songs = new Song[] {
+                new Song("アンパンマン", "バイキンマン", 60),
+                new Song("バイキンマン", "カレーパンマン", 60),
+                new Song("カレーパンマン", "しょくぱんまん", 60),
+                new Song("しょくぱんまん", "チーズ", 60),
+                new Song("チーズ", "バタコさん", 60),
+             };
+
         }
     }
 }
