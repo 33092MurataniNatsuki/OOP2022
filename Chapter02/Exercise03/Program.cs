61df7879ce74352e3b33732f71228c61d31db81e
33092村谷夏希 <33092@ojs.ac.jp>
2022-05-12 15:09:05 +0900

    問題2.3完成

--- Chapter02/Exercise03/Program.cs
+++ Chapter02/Exercise03/Program.cs
@@ -8,6 +8,25 @@ namespace Exercise03 {
     class Program {
         static void Main(string[] args)
         {
+
+            var sales = new SalesCounter("sales.csv");
+            Console.WriteLine("売上高の表示選択");
+            Console.WriteLine("1.店舗別");
+            Console.WriteLine("2.商品カテゴリー別");
+            var n = int.Parse(Console.ReadLine());
+            IDictionary<string, int> amountPerStore;
+            if (n == 1)
+            {
+                amountPerStore = sales.GetPerStoreSales();
+            }
+            else
+            {
+                amountPerStore = sales.GetPerProductSales();
+            }
+            foreach (var obj in amountPerStore)
+            {
+                Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+            }
         }
     }
 }
