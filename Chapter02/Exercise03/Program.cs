469d9c697945884b51946dc8a6dd1986af8b8d47
33092村谷夏希 <33092@ojs.ac.jp>
2022-05-12 15:57:02 +0900

    問題2.3完成

--- Chapter02/Exercise03/Program.cs
+++ Chapter02/Exercise03/Program.cs
@@ -8,21 +8,63 @@ namespace Exercise03 {
     class Program {
         static void Main(string[] args)
         {
+            #region
+            //var sales = new SalesCounter("sales.csv");
+            //Console.WriteLine("売上高の表示選択");
+            //Console.WriteLine("1.店舗別");
+            //Console.WriteLine("2.商品カテゴリー別");
+            //Console.Write(">");
 
+            //var n = int.Parse(Console.ReadLine());
+            //IDictionary<string, int> amountPerStore;
+
+
+            //if (n == 1)
+            //{
+            //    amountPerStore = sales.GetPerStoreSales();
+            //}
+            //else
+            //{
+            //    amountPerStore = sales.GetPerProductSales();
+            //}
+            //foreach (var obj in amountPerStore)
+            //{
+            //    Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+            //}
+            #endregion
+
+            int select = 0;
             var sales = new SalesCounter("sales.csv");
-            Console.WriteLine("売上高の表示選択");
-            Console.WriteLine("1.店舗別");
-            Console.WriteLine("2.商品カテゴリー別");
-            var n = int.Parse(Console.ReadLine());
-            IDictionary<string, int> amountPerStore;
-            if (n == 1)
+
+            // IDictionary<string, int> amountPerStore = null;
+
+            while (true)
             {
-                amountPerStore = sales.GetPerStoreSales();
+                Console.WriteLine("売上高の表示選択");
+                Console.WriteLine("1.店舗別");
+                Console.WriteLine("2.商品カテゴリー別");
+                Console.Write(">");
+
+                select = int.Parse(Console.ReadLine());
+
+                switch (select)
+                {
+                    case 1:
+                        OutPut(sales.GetPerStoreSales());
+                        break;
+
+                    case 2:
+                        OutPut(sales.GetPerProductSales());
+                        break;
+
+                    case 999:
+                        return;
+                }
             }
-            else
-            {
-                amountPerStore = sales.GetPerProductSales();
-            }
+        }
+
+        private static void OutPut(IDictionary<string, int> amountPerStore)
+        {
             foreach (var obj in amountPerStore)
             {
                 Console.WriteLine("{0} {1}", obj.Key, obj.Value);
