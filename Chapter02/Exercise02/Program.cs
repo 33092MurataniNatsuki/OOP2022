53051450dc6ee19445d18b65e3ada50174c224bb
33092村谷夏希 <33092@ojs.ac.jp>
2022-05-12 14:28:30 +0900

    問題2.2完成

--- Chapter02/Exercise02/Program.cs
+++ Chapter02/Exercise02/Program.cs
@@ -8,9 +8,20 @@ namespace Exercise02 {
     class Program {
         static void Main(string[] args)
         {
-            PrintInchToMeterList(1, 10);
+            //コマンドライン引数
+            if (args.Length >= 1 && args[0] == "-tom")
+            {
+                //インチからメートルへの対応表を出力
+                PrintInchToMeterList(1, 10);
+            }
+            else if (args.Length >= 1 && args[0] == "-toi")
+            {
+                //メートルからインチへの対応表を出力
+                PrintMeterToInchList(1, 10);
+            }
         }
 
+        //インチからメートルへの対応表を出力
         private static void PrintInchToMeterList(int start, int stop)
         {
             for (int inch = start; inch <= stop; inch++)
@@ -19,5 +30,16 @@ namespace Exercise02 {
                 Console.WriteLine("{0} inch = {1:0.0000} m", inch, meter);
             }
         }
+
+        //メートルからインチへの対応表を出力
+        private static void PrintMeterToInchList(int start, int stop)
+        {
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double feet = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} inch", meter, feet);
+            }
+        }
+
     }
 }
