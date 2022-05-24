3391bc7e5312e88dbf6d50dfe3b74a015a57686b
33092村谷夏希 <33092@ojs.ac.jp>
2022-05-24 13:27:53 +0900

    問題4.2作成準備

---
+++ Chapter04/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exercise01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            //問題4.1
+            //var ym = new YearMonth(2022, 5);
+
+            //var check = ym.Is21Century;
+            //var test = ym.AddOneMonth();
+
+            //Console.WriteLine(ym);
+        }
+    }
+}
