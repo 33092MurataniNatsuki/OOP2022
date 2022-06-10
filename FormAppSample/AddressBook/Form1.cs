fafed0e38973068494757379823fdde339c8db9b
33092MurataniNatsuki <104185427+33092MurataniNatsuki@users.noreply.github.com>
2022-06-10 15:58:26 +0900

    データグリッドビュー

--- FormAppSample/AddressBook/Form1.cs
+++ FormAppSample/AddressBook/Form1.cs
@@ -31,14 +31,89 @@ namespace AddressBook {
                 Address = tbAddress.Text,
                 Company = tbCompany.Text,
                 Picture = pbPicture.Image,
+                listGroup = GetCheckBoxGroup(),
             };
 
             listPerson.Add(newPerson);
 
         }
 
+        //チェックボックスにセットされている値をリストとして取り出す
+        private List<Person.GroupType> GetCheckBoxGroup() {
+            var listGroup = new List<Person.GroupType>();
+
+            if (cbFamily.Checked) {
+                listGroup.Add(Person.GroupType.家族);
+            }
+            if (cbFriend.Checked) {
+                listGroup.Add(Person.GroupType.友人);
+            }
+            if (cbWork.Checked) {
+                listGroup.Add(Person.GroupType.仕事);
+            }
+            if (cbOther.Checked) {
+                listGroup.Add(Person.GroupType.その他);
+            }
+
+            return listGroup;
+        }
+
         private void btPictureClear_Click(object sender, EventArgs e) {
             pbPicture.Image = null;
+        }
+
+        //データグリッドビューをクリックしたときのイベントハンドラ
+        private void dgvPersons_Click(object sender, EventArgs e) {
+            //例
+            //データグリッドビューのインデックス０番の名前をテキストボックスに格納
+            //tbName.Text = listPerson[0].Name;
+
+            //①選択されているインデックスを取得する
+            int index = dgvPersons.CurrentRow.Index;
+
+            //②インデックスが取得出来たら、リスト（listPerson）の該当するインデックスに
+            //  アクセスし、リストの各項目を各テキストボックスへ表示する
+            tbName.Text = listPerson[index].Name;
+            tbMailAddress.Text = listPerson[index].MailAddress;
+            tbAddress.Text = listPerson[index].Address;
+            tbCompany.Text = listPerson[index].Company;
+
+            pbPicture.Image = listPerson[index].Picture;
+
+            Clear();
+            foreach (var group in listPerson[index].listGroup) {
+                switch (group) {
+                    case Person.GroupType.家族:
+                        break;
+                    case Person.GroupType.友人:
+                        break;
+                    case Person.GroupType.仕事:
+                        break;
+                    case Person.GroupType.その他:
+                        break;
+                    default:
+                        break;
+                }
+
+                if (Person.GroupType.家族 == group) {
+                    cbFamily.Checked = true;
+                }
+                if (Person.GroupType.友人 == group) {
+                    cbFriend.Checked = true;
+                }
+                if (Person.GroupType.仕事 == group) {
+                    cbWork.Checked = true;
+                }
+                if (Person.GroupType.その他 == group) {
+                    cbOther.Checked = true;
+                }
+            }
+        }
+        private void Clear() {
+            cbFamily.Checked = false;
+            cbFriend.Checked = false;
+            cbWork.Checked = false;
+            cbOther.Checked = false;
         }
     }
 }
