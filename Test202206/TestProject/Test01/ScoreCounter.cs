08f161a9e1d99236cc32d2ab6e916d0a8b4cab57
33092MurataniNatsuki <104185427+33092MurataniNatsuki@users.noreply.github.com>
2022-06-14 14:29:40 +0900

    途中経過

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -8,39 +8,35 @@ namespace Test01 {
         // コンストラクタ
         public ScoreCounter(string filePath) {
             _score = ReadScore(filePath);
-
-
-            
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
-
-            
+            List<Student> students = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Student student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                students.Add(student);
+            }
+            return students;
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-            //var dict = new Dictionary<string, int>();
-            //foreach (var score in _score) {
-            //    if (dict.ContainsKey(score.ShopName))
-            //        dict[score.ShopName] += score.Amount;
-            //    else
-            //        dict[score.ShopName] = score.Amount;
-            //}
-            //return dict;
-
-
-
-
-
+            var dict = new Dictionary<string, int>();
+            foreach (var score in _score) {
+                if (dict.ContainsKey(score.Subject))
+                    dict[score.Subject] += score.Score;
+                else
+                    dict[score.Subject] = score.Score;
+            }
+            return dict;
         }
     }
 }
