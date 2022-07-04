4b166f118c4a74ce98ebf5830d2c237f70efa470
33092MurataniNatsuki <104185427+33092MurataniNatsuki@users.noreply.github.com>
2022-07-04 09:39:07 +0900

    完成

--- Chapter11/RssReader/Form1.Designer.cs
+++ Chapter11/RssReader/Form1.Designer.cs
@@ -27,22 +27,25 @@ namespace RssReader {
             this.cbRssUrl = new System.Windows.Forms.ComboBox();
             this.btRssGet = new System.Windows.Forms.Button();
             this.lbRssTitle = new System.Windows.Forms.ListBox();
-            this.wbBrowser = new System.Windows.Forms.WebBrowser();
+            this.btBack = new System.Windows.Forms.Button();
+            this.btFoward = new System.Windows.Forms.Button();
+            this.wvBrowser = new Microsoft.Toolkit.Forms.UI.Controls.WebView();
+            ((System.ComponentModel.ISupportInitialize)(this.wvBrowser)).BeginInit();
             this.SuspendLayout();
             // 
             // cbRssUrl
             // 
             this.cbRssUrl.FormattingEnabled = true;
-            this.cbRssUrl.Location = new System.Drawing.Point(12, 12);
+            this.cbRssUrl.Location = new System.Drawing.Point(12, 17);
             this.cbRssUrl.Name = "cbRssUrl";
-            this.cbRssUrl.Size = new System.Drawing.Size(807, 20);
+            this.cbRssUrl.Size = new System.Drawing.Size(694, 20);
             this.cbRssUrl.TabIndex = 0;
             // 
             // btRssGet
             // 
-            this.btRssGet.Location = new System.Drawing.Point(825, 10);
+            this.btRssGet.Location = new System.Drawing.Point(712, 15);
             this.btRssGet.Name = "btRssGet";
-            this.btRssGet.Size = new System.Drawing.Size(75, 23);
+            this.btRssGet.Size = new System.Drawing.Size(84, 23);
             this.btRssGet.TabIndex = 1;
             this.btRssGet.Text = "取得";
             this.btRssGet.UseVisualStyleBackColor = true;
@@ -50,6 +53,8 @@ namespace RssReader {
             // 
             // lbRssTitle
             // 
+            this.lbRssTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.lbRssTitle.FormattingEnabled = true;
             this.lbRssTitle.ItemHeight = 12;
             this.lbRssTitle.Location = new System.Drawing.Point(12, 48);
@@ -58,25 +63,55 @@ namespace RssReader {
             this.lbRssTitle.TabIndex = 2;
             this.lbRssTitle.Click += new System.EventHandler(this.lbRssTitle_Click);
             // 
-            // wbBrowser
+            // btBack
             // 
-            this.wbBrowser.Location = new System.Drawing.Point(260, 48);
-            this.wbBrowser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.wbBrowser.Name = "wbBrowser";
-            this.wbBrowser.Size = new System.Drawing.Size(640, 700);
-            this.wbBrowser.TabIndex = 3;
+            this.btBack.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btBack.Location = new System.Drawing.Point(802, 8);
+            this.btBack.Name = "btBack";
+            this.btBack.Size = new System.Drawing.Size(47, 30);
+            this.btBack.TabIndex = 4;
+            this.btBack.Text = "◀";
+            this.btBack.UseVisualStyleBackColor = true;
+            this.btBack.Click += new System.EventHandler(this.btBack_Click);
+            // 
+            // btFoward
+            // 
+            this.btFoward.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btFoward.Location = new System.Drawing.Point(855, 8);
+            this.btFoward.Name = "btFoward";
+            this.btFoward.Size = new System.Drawing.Size(45, 30);
+            this.btFoward.TabIndex = 4;
+            this.btFoward.Text = "▶";
+            this.btFoward.UseVisualStyleBackColor = true;
+            this.btFoward.Click += new System.EventHandler(this.btFoward_Click);
+            // 
+            // wvBrowser
+            // 
+            this.wvBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.wvBrowser.Location = new System.Drawing.Point(260, 48);
+            this.wvBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wvBrowser.Name = "wvBrowser";
+            this.wvBrowser.Size = new System.Drawing.Size(640, 700);
+            this.wvBrowser.TabIndex = 6;
+            this.wvBrowser.NavigationCompleted += new System.EventHandler<Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationCompletedEventArgs>(this.wvBrowser_NavigationCompleted);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(912, 759);
-            this.Controls.Add(this.wbBrowser);
+            this.ClientSize = new System.Drawing.Size(916, 759);
+            this.Controls.Add(this.wvBrowser);
+            this.Controls.Add(this.btFoward);
+            this.Controls.Add(this.btBack);
             this.Controls.Add(this.lbRssTitle);
             this.Controls.Add(this.btRssGet);
             this.Controls.Add(this.cbRssUrl);
             this.Name = "Form1";
             this.Text = "RssReader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.wvBrowser)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -86,7 +121,9 @@ namespace RssReader {
         private System.Windows.Forms.ComboBox cbRssUrl;
         private System.Windows.Forms.Button btRssGet;
         private System.Windows.Forms.ListBox lbRssTitle;
-        private System.Windows.Forms.WebBrowser wbBrowser;
+        private System.Windows.Forms.Button btBack;
+        private System.Windows.Forms.Button btFoward;
+        private Microsoft.Toolkit.Forms.UI.Controls.WebView wvBrowser;
     }
 }
 
--- Chapter11/RssReader/Form1.cs
+++ Chapter11/RssReader/Form1.cs
@@ -12,14 +12,14 @@ using System.Net;
 
 namespace RssReader {
     public partial class Form1 : Form {
-        IEnumerable<string> xTitle;
-        IEnumerable<string> xLink;
+        IEnumerable<string> xTitle, xLink;
 
         public Form1() {
             InitializeComponent();
         }
 
         private void btRssGet_Click(object sender, EventArgs e) {
+            lbRssTitle.Items.Clear();
             using (var wc = new WebClient()) {
                 var stream = wc.OpenRead(cbRssUrl.Text);
 
@@ -30,15 +30,39 @@ namespace RssReader {
                 foreach (var data in xTitle) {
                     lbRssTitle.Items.Add(data);
                 }
-                foreach (var data in xLink) {
-                    lbRssTitle.Items.Add(data);
-                }
             }
         }
 
-        private void lbRssTitle_Click(object sender, EventArgs e) {
-            int index =lbRssTitle.SelectedIndex; //選択した箇所のインデックスを取得（0～　）
+        private void btBack_Click(object sender, EventArgs e) {
+            wvBrowser.GoBack();
+        }
 
+        private void btFoward_Click(object sender, EventArgs e) {
+            wvBrowser.GoForward();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            backForwardBottonMaskCheck();
+        }
+
+        private void wvBrowser_NavigationCompleted(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationCompletedEventArgs e) {
+            backForwardBottonMaskCheck();
+        }
+
+        private void backForwardBottonMaskCheck() {
+            //進む・戻るボタンのマスク設定
+            btBack.Enabled = wvBrowser.CanGoBack;
+            btFoward.Enabled = wvBrowser.CanGoForward;
+        }
+
+        private void lbRssTitle_Click(object sender, EventArgs e) {
+            int index = lbRssTitle.SelectedIndex; //選択した箇所のインデックスを取得（0～　）
+            if (index == -1) return;
+
+            var url = xLink.ElementAt(index);
+            //wbBrowser.Navigate(url);
+            //wbBrowser.Url = new Uri(url);
+            wvBrowser.Source = new Uri(url);
         }
     }
 }
