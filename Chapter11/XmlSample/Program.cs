8cfb91b5e68e020df80339bc8c3f4dfca052fb2e
33092MurataniNatsuki <104185427+33092MurataniNatsuki@users.noreply.github.com>
2022-07-04 11:56:47 +0900

    途中

--- Chapter11/XmlSample/Program.cs
+++ Chapter11/XmlSample/Program.cs
@@ -9,20 +9,20 @@ using System.Net;
 namespace XmlSample {
     class Program {
         static void Main(string[] args) {
-            using (var wc = new WebClient()) {
-                var stream = wc.OpenRead("https://news.yahoo.co.jp/rss/media/jomo/all.xml");
 
-                var xdoc = XDocument.Load(stream);
-                var xTitle = xdoc.Root.Descendants("item").Select(x => x.Element("title"));
-                var xLink = xdoc.Root.Descendants("item").Select(x => x.Element("title"));
-
-                foreach (var data in xTitle) {
-                    Console.WriteLine(data);
-                }
-                foreach (var data in xLink) {
-                    Console.WriteLine(data);
-                }
+            var xdoc = XDocument.Load("novelists.xml");
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new {
+                                    Name = (string)x.Element("name"),
+                                    Birth = (DateTime)x.Element("birth"),
+                                    Death = (DateTime)x.Element("death")
+                                });
+            foreach (var novelist in novelists) {
+                Console.WriteLine("{0} ({1}-{2})",
+                                   novelist.Name, novelist.Birth.Year, novelist.Death.Year);
             }
+
         }
     }
 }
+
