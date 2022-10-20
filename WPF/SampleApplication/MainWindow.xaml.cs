cac708ef02d311e8f9be64864b207eb2f4e258a4
33092MurataniNatsuki <104185427+33092MurataniNatsuki@users.noreply.github.com>
2022-10-20 11:18:33 +0900

    SampleSlider完成

--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -20,14 +20,45 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            seasonComboBox.SelectedIndex = 0;
+            getSeasonIndex();
         }
 
-        private void okButton_Click(object sender, RoutedEventArgs e) {
-
+        private void getSeasonIndex() {
+            DateTime dt = DateTime.Now;
+            if (3 <= dt.Month && dt.Month <= 5) {
+                seasonComboBox.SelectedIndex = 0;
+            } else if (6 <= dt.Month && dt.Month <= 8) {
+                seasonComboBox.SelectedIndex = 1;
+            } else if (9 <= dt.Month && dt.Month <= 11) {
+                seasonComboBox.SelectedIndex = 2;
+            } else if (12 <= dt.Month && dt.Month <= 2) {
+                seasonComboBox.SelectedIndex = 3;
+            }
         }
 
-        private void cancelButton_Click(object sender, RoutedEventArgs e) {
+        private void checkBox_Checked(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "チェック済み";
+        }
 
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "未チェック";
+        }
+
+        private void seasonComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            seasonTextBlock.Text = (string)((ComboBoxItem)seasonComboBox.SelectedItem).Content;
+        }
+
+        private void yellowRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "黄";
+        }
+
+        private void blueRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "青";
+        }
+
+        private void redRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "赤";
         }
     }
 }
