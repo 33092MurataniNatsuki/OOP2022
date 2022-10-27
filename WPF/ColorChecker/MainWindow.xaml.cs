74f9179bc611ecd0917a3c355ef38a47fa4428a5
33092MurataniNatsuki <104185427+33092MurataniNatsuki@users.noreply.github.com>
2022-10-27 11:49:25 +0900

    ColorChecker完成

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -21,5 +21,30 @@ namespace ColorChecker {
         public MainWindow() {
             InitializeComponent();
         }
+
+        public void chengeSlider() {
+            if (rValue != null && gValue != null && bValue != null) {
+                var R = (int)Math.Round(double.Parse(rValue.Text));
+                var G = (int)Math.Round(double.Parse(gValue.Text));
+                var B = (int)Math.Round(double.Parse(bValue.Text));
+                rValue.Text = R.ToString();
+                gValue.Text = G.ToString();
+                bValue.Text = B.ToString();
+                Color color = Color.FromRgb((byte)R, (byte)G, (byte)B);
+                colorLabel.Background = new SolidColorBrush(color);
+            }
+        }
+
+        private void rValue_TextChanged(object sender, TextChangedEventArgs e) {
+            chengeSlider();
+        }
+
+        private void gValue_TextChanged(object sender, TextChangedEventArgs e) {
+            chengeSlider();
+        }
+
+        private void bValue_TextChanged(object sender, TextChangedEventArgs e) {
+            chengeSlider();
+        }
     }
 }
