56b97b813cb1c94191b0805d7b4c930245c7c837
33092MurataniNatsuki <104185427+33092MurataniNatsuki@users.noreply.github.com>
2022-10-31 14:28:25 +0900

    途中

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -19,13 +19,16 @@ namespace ColorChecker {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+
+        List<MyColor> stockMyColor = new List<MyColor>();
+
         public MainWindow() {
             InitializeComponent();
             DataContext = GetColorList();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e) {
-            colorLabel.Background = Brushes.Black;
+            setColor();
         }
 
         /// <summary>
@@ -64,15 +67,41 @@ namespace ColorChecker {
             chengeSlider();
         }
 
+        //テキストボックスの値を元に色を設定
+        private void setColor() {
+            var r = byte.Parse(rValue.Text);
+            var g = byte.Parse(gValue.Text);
+            var b = byte.Parse(bValue.Text);
+            colorLabel.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+
         private void uxColorSelect_SelectionChanged(object sender, SelectionChangedEventArgs e) {
             var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
             var color = mycolor.Color;
-            var name = mycolor.Name;
             colorLabel.Background = new SolidColorBrush(color);
 
             rValue.Text = mycolor.Color.R.ToString();
-            gValue.Text = mycolor.Color.R.ToString();
-            bValue.Text = mycolor.Color.R.ToString();
+            gValue.Text = mycolor.Color.G.ToString();
+            bValue.Text = mycolor.Color.B.ToString();
+
+            stockMyColor.Add(new MyColor() { Color = color, Name = "R:" + rValue.Text + " " + "G:" + gValue.Text + " " + "B:" + bValue.Text });
+            stockList.Items.Add(mycolor.Name + " " + "R:" + rValue.Text + "G:" + gValue.Text + "B:" + bValue.Text);
+        }
+
+        private void stock_Click(object sender, RoutedEventArgs e) {
+            if (uxColorSelect.SelectedItem == null) {
+                stockMyColor.Add(new MyColor() { Name = "R:" + rValue.Text + " " + "G:" + gValue.Text + " " + "B:" + bValue.Text });
+                stockList.Items.Add("R:" + rValue.Text + "G:" + gValue.Text + "B:" + bValue.Text);
+            } else {
+                //stockMyColor.Add(new MyColor() { Color =  , Name = "R:" + rValue.Text + " " + "G:" + gValue.Text + " " + "B:" + bValue.Text });
+                //stockList.Items.Add(uxColorSelect.SelectedValue + " " + "R:" + rValue.Text + "G:" + gValue.Text + "B:" + bValue.Text);
+            }
+
+        }
+
+        private void delete_Click(object sender, RoutedEventArgs e) {
+            //int sel = (int)stockList.SelectedItem;
+            //stockList.Items.RemoveAt(sel);
         }
     }
 
